c70bc8e792b815feea9a297a8af659aae171008f
Dania <27072003danya@gmail.com>
2023-03-02 20:55:23 +0200

    add new links for files

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-
-fetch("http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19")
-.then(response => response.json())
-.then(data => {
+fetch(
+  "http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19"
+)
+  .then((response) => response.json())
+  .then((data) => {
     let city = document.querySelector(".city_name");
     let temperature = document.querySelector(".temp");
     let pressure = document.querySelector(".pressure");
@@ -14,15 +15,11 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=
     city.innerHTML = data.name;
     temperature.innerHTML = `Current temperature: ${data.main.temp} °C`;
     pressure.innerHTML = `Pressure: ${data.main.pressure} Pa`;
-    data.weather.forEach(item => {
-        description.innerHTML = `Description: ${item.description}`;
+    data.weather.forEach((item) => {
+      description.innerHTML = `Description: ${item.description}`;
     });
     humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
     speed.innerHTML = `Speed: ${data.wind.speed} Km/h`;
     deg.innerHTML = `Deg: ${data.wind.deg}°`;
     img.src = "http://openweathermap.org/img/w/04d.png";
-});
-
-fetch("http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19")
-.then(response => response.json())
-.then(data => console.log(data))
+  });
